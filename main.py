3abc628405ec9246151c046304fa0b77890ad8eb
340234-spec <340234@apps.wilsonareasd.org>
2025-09-26 13:23:29 +0000

    fixed server

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import eventlet
+eventlet.monkey_patch()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
